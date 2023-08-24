48f91a1a5bdaa7db3504e2f39004fd74c5a5f3d5
Adarsh rawat <00adarshrawat00@gmail.com>
2023-08-24 17:23:26 +0530

    React menu with useState

--- client/components/navbar/Navbar.jsx
+++ client/components/navbar/Navbar.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Navbar.scss"
 import { Link } from "react-router-dom"
 
 function Navbar() {
+
+    const [open, setOpen] = useState(false);
+
+    const currentUser = {
+        id:1,
+        usename:"john Doe",
+        isSeller:true
+    }
+
     return (
         <div className='navbar'>
             <div className="container">
@@ -17,8 +26,27 @@ function Navbar() {
                     <span>Explore</span>
                     <span>English</span>
                     <span>Sign in</span>
-                    <span>Become a Seller</span>
-                    <button>Join</button>
+                    {!currentUser?.isSeller && <span>Become a Seller</span>}
+                    {!currentUser && <button>Join</button>}
+                    {currentUser && (
+                        <div className="user" onClick={()=>setOpen(!open)}>
+                            <img src="" alt="" />
+                            <span>{currentUser.usename}</span>
+                            { open && (
+                                <div className="option">
+                                {currentUser?.isSeller && (
+                                        <>
+                                            <span>Gigs</span>
+                                            <span>Add new Gig</span>
+                                        </>
+                                )}  
+                                <span>Orders</span>                            
+                                <span>Messages</span>                            
+                                <span>Logout</span>                            
+                            </div>
+                            )}
+                        </div>
+                    )}
                 </div>
             </div>
             <hr />
