79fc575f2e769eb37b72520423f57bf85e717f6b
Adarsh rawat <00adarshrawat00@gmail.com>
2023-08-25 06:50:45 +0530

    featured component design

--- client/pages/home/Home.jsx
+++ client/pages/home/Home.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Featured from '../../components/featured/Featured';
 
 function Home() {
   return (
-    <div className='home'>Home</div>
+    <div className='home'>
+      <Featured/>
+    </div>
   )
 }
 
