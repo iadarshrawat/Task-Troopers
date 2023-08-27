63566f7746f59508f325203449d56df82a09b198
Adarsh rawat <00adarshrawat00@gmail.com>
2023-08-27 17:06:54 +0530

    Feature Component Design

--- client/pages/home/Home.jsx
+++ client/pages/home/Home.jsx
@@ -3,6 +3,7 @@ import Featured from '../../components/featured/Featured';
 import Slide from '../../components/Slide/Slide';
 import { cards } from '../../src/data.js';
 import CatCard from '../../components/catCard/CatCard';
+import './Home.scss'
 
 function Home() {
   return (
@@ -15,6 +16,52 @@ function Home() {
           ))
         }
       </Slide>
+
+        <div className="features">
+          <div className="container">
+            <div className="item">
+            <h1>A whole world of freelance talent at your fingertips</h1>
+            <div className="title">
+              <img src="./img/check.png" alt=""/>
+              The best for every budget
+            </div>
+            <p>
+              Find high-quality services at every price point. No hourly rates, just project-based pricing.
+            </p>
+            <div className="title">
+              <img src="./img/check.png" alt=""/>
+              The best for every budget
+            </div>
+            <p>
+              Find high-quality services at every price point. No hourly rates, just project-based pricing.
+            </p>
+            <div className="title">
+              <img src="./img/check.png" alt=""/>
+              The best for every budget
+            </div>
+            <p>
+              Find high-quality services at every price point. No hourly rates, just project-based pricing.
+            </p>
+            <div className="title">
+              <img src="./img/check.png" alt=""/>
+              The best for every budget
+            </div>
+            <p>
+              Find high-quality services at every price point. No hourly rates, just project-based pricing.
+            </p>
+            <div className="title">
+              <img src="./img/check.png" alt=""/>
+              The best for every budget
+            </div>
+            <p>
+              Find high-quality services at every price point. No hourly rates, just project-based pricing.
+            </p>
+            </div>
+            <div className="item">
+              <video src="./img/video.mp4" controls></video>
+            </div>
+          </div>
+        </div>
 
     </div>
   )
