295e7b90caa77ae2d58d23b869562638e173aaef
Adarsh rawat <00adarshrawat00@gmail.com>
2023-08-28 21:56:47 +0530

    react product slider

--- client/pages/home/Home.jsx
+++ client/pages/home/Home.jsx
@@ -2,67 +2,77 @@ import React from 'react'
 import Featured from '../../components/featured/Featured';
 import Slide from '../../components/Slide/Slide';
 import { cards } from '../../src/data.js';
+import { projects } from '../../src/data.js';
 import CatCard from '../../components/catCard/CatCard';
 import './Home.scss'
+import ProjectCard from '../../components/projectCard/ProjectCard';
 
 function Home() {
   return (
     <div className='home'>
-      <Featured/>
+      <Featured />
       <Slide slidesToShow={5} arrowsScroll={5}>
         {
-          cards.map(card=>(
-            <CatCard key={card.id} item={card} />      
+          cards.map(card => (
+            <CatCard key={card.id} item={card} />
           ))
         }
       </Slide>
 
-        <div className="features">
-          <div className="container">
-            <div className="item">
+      <div className="features">
+        <div className="container">
+          <div className="item">
             <h1>A whole world of freelance talent at your fingertips</h1>
             <div className="title">
-              <img src="./img/check.png" alt=""/>
+              <img src="./img/check.png" alt="" />
               The best for every budget
             </div>
             <p>
               Find high-quality services at every price point. No hourly rates, just project-based pricing.
             </p>
             <div className="title">
-              <img src="./img/check.png" alt=""/>
+              <img src="./img/check.png" alt="" />
               The best for every budget
             </div>
             <p>
               Find high-quality services at every price point. No hourly rates, just project-based pricing.
             </p>
             <div className="title">
-              <img src="./img/check.png" alt=""/>
+              <img src="./img/check.png" alt="" />
               The best for every budget
             </div>
             <p>
               Find high-quality services at every price point. No hourly rates, just project-based pricing.
             </p>
             <div className="title">
-              <img src="./img/check.png" alt=""/>
+              <img src="./img/check.png" alt="" />
               The best for every budget
             </div>
             <p>
               Find high-quality services at every price point. No hourly rates, just project-based pricing.
             </p>
             <div className="title">
-              <img src="./img/check.png" alt=""/>
+              <img src="./img/check.png" alt="" />
               The best for every budget
             </div>
             <p>
               Find high-quality services at every price point. No hourly rates, just project-based pricing.
             </p>
-            </div>
-            <div className="item">
-              <video src="./img/video.mp4" controls></video>
-            </div>
+          </div>
+          <div className="item">
+            <video src="./img/video.mp4" controls></video>
           </div>
         </div>
+      </div>
 
+
+      <Slide slidesToShow={5} arrowsScroll={5}>
+        {
+          projects.map(card => (
+            <ProjectCard key={card.id} item={card} />
+          ))
+        }
+      </Slide>
     </div>
   )
 }
