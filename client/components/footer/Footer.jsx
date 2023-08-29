1e9d27863b34dea6c74d19d985dc88cc440a73da
Adarsh rawat <00adarshrawat00@gmail.com>
2023-08-29 08:34:43 +0530

    react footer design

--- client/components/footer/Footer.jsx
+++ client/components/footer/Footer.jsx
@@ -3,7 +3,78 @@ import "./Footer.scss"
 
 function Footer() {
   return (
-    <div className='footer'>Footer</div>
+    <div className='footer'>
+      <div className="container">
+        <div className="top">
+          <div className="item">
+            <h2>Categories</h2>
+            <span>Graphics & Design</span>
+            <span>Digital Marketing</span>
+            <span>Writing & Translation</span>
+            <span>Video & Animation</span>
+            <span>Music & Audio</span>
+            <span>Programming & Tech</span>
+            <span>Photography</span>
+          </div>
+          <div className="item">
+            <h2>Categories</h2>
+            <span>Graphics & Design</span>
+            <span>Digital Marketing</span>
+            <span>Writing & Translation</span>
+            <span>Video & Animation</span>
+            <span>Music & Audio</span>
+            <span>Programming & Tech</span>
+            <span>Photography</span>
+          </div>
+          <div className="item">
+            <h2>Categories</h2>
+            <span>Graphics & Design</span>
+            <span>Digital Marketing</span>
+            <span>Writing & Translation</span>
+            <span>Video & Animation</span>
+            <span>Music & Audio</span>
+            <span>Programming & Tech</span>
+            <span>Photography</span>
+          </div>
+          <div className="item">
+            <h2>Categories</h2>
+            <span>Graphics & Design</span>
+            <span>Digital Marketing</span>
+            <span>Writing & Translation</span>
+            <span>Video & Animation</span>
+            <span>Music & Audio</span>
+            <span>Programming & Tech</span>
+            <span>Photography</span>
+          </div>
+        </div>
+        <div className="bottom">
+          <div className="left">
+            <h2>Fiverr</h2>
+            <span>Fiverr International LTD. 2023</span>
+          </div>
+          <div className="right">
+            <div className="social">
+              <img src="img/twitter.png" alt="" />
+              <img src="img/facebook.png" alt="" />
+              <img src="img/linkedin.png" alt="" />
+              <img src="img/pinterest.png" alt="" />
+            </div>
+            <div className="link">
+              <img src="./img/language.png" alt="" />
+              <span>English</span>
+            </div>
+            <div className="link">
+              <img src="./img/coin.png" alt="" />
+              <span>USD</span>
+            </div>
+            <div className="link">
+              <img src="./img/accessibility.png" alt="" />
+              <span>English</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
