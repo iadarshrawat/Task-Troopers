11428a9d0c8d4f2de6df4b9c67b3aa2495b26f29
Adarsh rawat <00adarshrawat00@gmail.com>
2023-09-03 07:38:23 +0530

    reinitialised backend

--- routes/auth.route.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-import { login, logout, register } from '../controller/auth.controller.js';
-const router = express.Router();
-
-router.post('/register', register);
-router.post('/login', login);
-router.post('/logout', logout);
-
-export default router;
--- routes/user.route.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-import { deleteUser } from '../controller/user.controller.js';
-import { verifyToken } from '../middleware/jwt.js';
-
-const router = express.Router();
-
-router.delete('/:id', verifyToken ,deleteUser);
-
-export default router; 
